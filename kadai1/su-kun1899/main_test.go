1c66498afd5d14ce127a3f499fd8a99afbb886b3
su-kun1899 <higedrum.coz@gmail.com>
2018-11-07 22:07:19 +0900

    画像フォーマットのチェック

--- kadai1/su-kun1899/main_test.go
+++ kadai1/su-kun1899/main_test.go
@@ -1,7 +1,44 @@
 package main_test
 
-import "testing"
+import (
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	_ "image/gif"
+	"os"
+	"reflect"
+	"testing"
+)
 
-func Test_main(t *testing.T) {
-	println("Hello test!")
+func Test_format(t *testing.T) {
+	tests := []struct {
+		name     string
+		fileName string
+		want     string
+	}{
+		{"gif", "testdata/syokuji_computer.gif", "gif"},
+		{"png", "testdata/syokuji_computer.png", "png"},
+		{"jpeg", "testdata/syokuji_computer.jpg", "jpeg"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			reader, err := os.Open(test.fileName)
+			if err != nil {
+				t.Error("unexpected error:", err)
+				return
+			}
+			defer reader.Close()
+
+			_, got, err := image.Decode(reader)
+			if err != nil {
+				t.Error("unexpected error:", err)
+				return
+			}
+
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("Format = %v, want %v", got, test.want)
+				return
+			}
+		})
+	}
 }

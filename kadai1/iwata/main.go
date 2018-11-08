2698b385e6776d662fd44171d51e5b46e50710f1
iwata <121048+iwata@users.noreply.github.com>
2018-11-08 23:17:59 +0900

    :memo: (write a package document)

--- kadai1/iwata/main.go
+++ kadai1/iwata/main.go
@@ -1,3 +1,21 @@
+/*
+特定ディレクトリの画像を再帰的に辿って、ファイルフォーマットを変換するCLIです.
+
+How to build
+
+	env GO111MODULE=on go build -o imgconv
+
+Usage
+
+Usage of ./imgconv:
+   ./imgconv [OPTIONS] DIR
+OPTIONS
+  -from string
+    Convert from image format (default "jpg")
+  -to string
+    Convert to image format (default "png")
+
+*/
 package main
 
 import (

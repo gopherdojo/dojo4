379ecf076b7da89a4c333e69067c91e442293d13
su-kun1899 <higedrum.coz@gmail.com>
2018-11-08 22:44:38 +0900

    引数を受け取るようにする

--- kadai1/su-kun1899/main.go
+++ kadai1/su-kun1899/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, dojo")
+	os.Exit(runCmd(os.Args[1:]))
 }
+
+func runCmd(args []string) int {
+	if len(args) != 1 {
+		fmt.Fprintln(os.Stderr, errors.New("missing target directory"))
+		return 1
+	}
+
+	targetDir := args[0]
+	println(targetDir)
+
+	return 0
+}
+
 
 func replaceExt(fileName, newExt string) string {
 	return fmt.Sprintf("%s.%s", strings.TrimSuffix(fileName, filepath.Ext(fileName)), newExt)

6ea099150d1661048ce8a8616721e69efa259657
su-kun1899 <higedrum.coz@gmail.com>
2018-11-08 21:23:01 +0900

    検証用のコードを削除

--- kadai1/su-kun1899/main_test.go
+++ kadai1/su-kun1899/main_test.go
@@ -2,79 +2,13 @@ package main_test
 
 import (
 	"fmt"
-	"image"
 	_ "image/gif"
 	_ "image/jpeg"
-	"image/png"
 	_ "image/png"
-	"os"
 	"path/filepath"
-	"reflect"
 	"strings"
 	"testing"
 )
-
-func Test_format(t *testing.T) {
-	tests := []struct {
-		name     string
-		fileName string
-		want     string
-	}{
-		{"gif", filepath.Join("testdata", "syokuji_computer.gif"), "gif"},
-		{"png", filepath.Join("testdata", "syokuji_computer.png"), "png"},
-		{"jpeg", filepath.Join("testdata", "syokuji_computer.jpg"), "jpeg"},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			reader, err := os.Open(test.fileName)
-			if err != nil {
-				t.Error("unexpected error:", err)
-				return
-			}
-			defer reader.Close()
-
-			_, got, err := image.Decode(reader)
-			if err != nil {
-				t.Error("unexpected error:", err)
-				return
-			}
-
-			if !reflect.DeepEqual(got, test.want) {
-				t.Errorf("Format = %v, want %v", got, test.want)
-				return
-			}
-		})
-	}
-}
-
-func Test_convert(t *testing.T) {
-	reader, err := os.Open(filepath.Join("testdata", "syokuji_computer.jpg"))
-	if err != nil {
-		t.Error("unexpected error:", err)
-		return
-	}
-	defer reader.Close()
-
-	img, _, err := image.Decode(reader)
-	if err != nil {
-		t.Error("unexpected error:", err)
-		return
-	}
-
-	writer, err := os.Create("testdata/writer.png")
-	if err != nil {
-		t.Error("unexpected error:", err)
-		return
-	}
-	defer writer.Close()
-	defer os.Remove("testdata/writer.png")
-
-	err = png.Encode(writer, img)
-	if err != nil {
-		t.Error("unexpected error:", err)
-		return
-	}
-}
 
 func Test_convertExt(t *testing.T) {
 	before := filepath.Join("testdata", "syokuji_computer.jpg")

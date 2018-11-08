d626c64ae1d58b4a0dccbacac7f3beb7bf0e1f51
su-kun1899 <higedrum.coz@gmail.com>
2018-11-08 22:24:28 +0900

    不要な処理を削除

--- kadai1/su-kun1899/main_test.go
+++ kadai1/su-kun1899/main_test.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"fmt"
-	_ "image/gif"
-	_ "image/jpeg"
-	_ "image/png"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 )
-
-func Test_convertExt(t *testing.T) {
-	before := filepath.Join("testdata", "syokuji_computer.jpg")
-	want := filepath.Join("testdata", "syokuji_computer.png")
-	got := fmt.Sprintf("%s.%s", strings.TrimSuffix(before, filepath.Ext(before)), "png")
-
-	if got != want {
-		t.Errorf("got = %v, want %v", got, want)
-		return
-	}
-}
 
 func TestReplaceExt(t *testing.T) {
 	type args struct {

2a92cd91cb6f710c4c35458b84aa61de623aab30
iwata <121048+iwata@users.noreply.github.com>
2018-11-08 11:38:33 +0900

    :recycle: (export some methods to filter modules)

--- kadai1/iwata/main.go
+++ kadai1/iwata/main.go
@@ -3,18 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/iwata/dojo4/kadai1/iwata/cmdparser"
 	"github.com/iwata/dojo4/kadai1/iwata/converter"
+	"github.com/iwata/dojo4/kadai1/iwata/filter"
 )
-
-type filterConfig interface {
-	SrcDir() string
-	FromFormat() string
-}
 
 func main() {
 	c, err := cmdparser.Parse()
@@ -22,7 +15,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	files, err := filterFiles(c)
+	files, err := filter.Files(c)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,24 +25,4 @@ func main() {
 	}
 
 	fmt.Printf("Converted %d %s files to %s in %s successfully\n", len(files), c.FromFormat(), c.ToFormat(), c.SrcDir())
-}
-
-func filterFiles(c filterConfig) ([]string, error) {
-	var files []string
-	ext := fmt.Sprintf(".%s", c.FromFormat())
-
-	err := filepath.Walk(c.SrcDir(), func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			return nil
-		}
-		if strings.HasSuffix(path, ext) {
-			files = append(files, path)
-		}
-		return nil
-	})
-
-	return files, err
 }

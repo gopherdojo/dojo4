d524a1dff245b861b46f316747a652ed822bdbcc
YoheiMiyamoto <yoheimiyamoto@gmail.com>
2018-11-17 15:32:03 +0900

    add: test

---
+++ kadai2/yoheimiyamoto/image-convert/convert_test.go
@@ -0,0 +1,69 @@
+package imageconvert
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+const dirPath = "test-img"
+const fileName = "img.jpg"
+
+var filePath = fmt.Sprintf("%s/%s", dirPath, fileName)
+
+func TestChangeExt(t *testing.T) {
+	expect := "/path/hello.git"
+	actual := changeExt("/path/hello.jpg", "git")
+	if actual != expect {
+		t.Errorf(`expect="%s", actual="%s"`, expect, actual)
+	}
+}
+
+func TestFileWalk(t *testing.T) {
+	expect := filePath
+	var actual string
+
+	err := fileWalk(dirPath, "jpg", func(path string) error {
+		actual = path
+		return nil
+	})
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if actual != expect {
+		t.Errorf(`expect="%s", actual="%s"`, expect, actual)
+	}
+}
+
+func TestConverts(t *testing.T) {
+	destFormats := []string{"png", "gif"}
+	for _, f := range destFormats {
+		err := Converts(dirPath, "jpg", f)
+		if err != nil {
+			t.Errorf(err.Error())
+			return
+		}
+		_, err = os.Open(changeExt(filePath, f)) // ファイルが存在していることを確認
+		if err != nil {
+			t.Errorf(err.Error())
+			return
+		}
+		os.Remove(changeExt(filePath, f))
+	}
+}
+
+// テスト用のjpegファイルを作成
+// func createJPEG(filePath string) error {
+// 	err := os.Mkdir(dirPath, 0777)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+// 	f, err := os.Create(filePath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+// 	return jpeg.Encode(f, img, nil)
+// }

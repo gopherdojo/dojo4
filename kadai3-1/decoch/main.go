97659b0419c38c23811df11a14e42064730a55f3
decoch <tominaga.switch@gmail.com>
2018-11-25 12:09:52 +0900

    sync score.

--- kadai3-1/decoch/main.go
+++ kadai3-1/decoch/main.go
@@ -8,54 +8,72 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
 func main() {
-	fmt.Println("typing game start.")
+	fmt.Println("### typing game start. ###")
 
-	gameCh := make(chan struct{})
-	timerCh := make(chan struct{})
+	score := 0
 
-	go startGame(gameCh, timerCh)
+	ch1 := input()
+	ch2 := timer()
 
-	time.Sleep(10 * time.Second)
-	close(timerCh)
-
-	<-gameCh // wait until game end.
-	fmt.Println("end typing game.")
+	var m sync.Mutex
+	select {
+	case v1 := <-ch1:
+		m.Lock()
+		if v1 {
+			score++
+		}
+		m.Unlock()
+	case _ = <-ch2:
+		m.Lock()
+		fmt.Printf("\n### Score: %d ###\n", score)
+		fmt.Println("### typing game end. ###")
+		m.Unlock()
+	}
 }
 
-func startGame(gameCh, timerCh chan struct{}) {
-	result := 0
-
+func input() <-chan bool {
+	inputCh := make(chan bool)
 	go func() {
-		<-timerCh
-		fmt.Printf("score: %d\n", result)
-		close(gameCh)
-	}()
-
-	words, err := getWords()
-	if err != nil {
-		log.Fatalln("cannot get words.", err)
-	}
-
-	stdin := bufio.NewScanner(os.Stdin)
-	rand.Seed(time.Now().UnixNano())
-	for {
-		position := rand.Intn(len(words))
-		word := words[position]
-		fmt.Println(word)
-
-		stdin.Scan()
-		input := stdin.Text()
-		if word == input {
-			fmt.Printf("### GOT IT!!!! ###\n\n")
-			result++
-		} else {
-			fmt.Printf("### WRONG ###\n\n")
+		words, err := getWords()
+		if err != nil {
+			log.Fatalln("cannot get words.", err)
 		}
-	}
+
+		stdin := bufio.NewScanner(os.Stdin)
+		rand.Seed(time.Now().UnixNano())
+		for {
+			position := rand.Intn(len(words))
+			word := words[position]
+			fmt.Println(word)
+
+			stdin.Scan()
+			input := stdin.Text()
+			if word == input {
+				fmt.Printf("### GOT IT!!!! ###\n\n")
+				inputCh <- true
+			} else {
+				fmt.Printf("### WRONG ###\n\n")
+				inputCh <- false
+			}
+		}
+		close(inputCh)
+	}()
+	return inputCh
+}
+
+func timer() <-chan struct{} {
+	timerCh := make(chan struct{})
+	go func() {
+		time.Sleep(2 * time.Second)
+		timerCh <- struct{}{}
+		close(timerCh)
+	}()
+	return timerCh
 }
 
 func getWords() ([]string, error) {

bcb4b4a07db4bb6b3a93dfc63684fa3d9f3979ab
YoheiMiyamoto <yoheimiyamoto@gmail.com>
2018-12-02 09:18:39 +0900

    update

---
+++ kadai3-2/yoheimiyamoto/downloader/client.go
@@ -0,0 +1,150 @@
+package downloader
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// Client ...
+type Client struct {
+	*http.Client
+	url string
+}
+
+// NewClient ...
+func NewClient(url string) *Client {
+	return &Client{new(http.Client), url}
+}
+
+// Download ...
+func (c *Client) Download(ctx context.Context) error {
+	l, err := c.contentLength()
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(os.Stdout, "contentLength: %d\n", l)
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	ps := newRangeProperties(l)
+	fmt.Fprintf(os.Stdout, "ps: %d\n", len(ps))
+
+	for _, p := range ps {
+		p := p
+		eg.Go(func() error {
+			return c.rangeDownload(ctx, p)
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+
+	defer removeFiles(ps)
+
+	src := make([]string, len(ps))
+	for i, p := range ps {
+		src[i] = p.path
+	}
+	err = mergeFiles(src, "output.jpg")
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+// contentLengthを取得
+func (c *Client) contentLength() (int, error) {
+	res, err := c.Head(c.url)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(res.Header.Get("Content-Length"))
+}
+
+// ファイルの分割ダウンロード
+// start -> 開始のbyte
+// end -> 終了のbyte
+// dst -> ダウンロード先のファイル名
+func (c *Client) rangeDownload(ctx context.Context, r *rangeProperty) error {
+	errCh := make(chan error, 1)
+
+	go func() {
+		req, _ := http.NewRequest("GET", c.url, nil)
+		req.Header.Set("Authorization", "Bearer access-token")
+		req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", r.start, r.end))
+
+		res, err := c.Do(req)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		defer res.Body.Close()
+
+		f, err := os.Create(r.path)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		defer f.Close()
+		_, err = io.Copy(f, res.Body)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		errCh <- nil
+	}()
+	select {
+	case err := <-errCh:
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+	case <-ctx.Done():
+		fmt.Println("強制終了")
+		return ctx.Err()
+	}
+
+	return nil
+}
+
+// ダウンロードした分割ファイルのマージ
+// src -> 元の分割ファイル名
+// dst -> マージ先のファイル名
+func mergeFiles(src []string, dst string) error {
+	f, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	for _, s := range src {
+		_f, err := os.Open(s)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(f, _f)
+		if err != nil {
+			return err
+		}
+		_f.Close()
+	}
+	return nil
+}
+
+// 一時保存ファイルの全削除
+func removeFiles(ps []*rangeProperty) error {
+	for _, p := range ps {
+		err := os.Remove(p.path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- kadai3-2/yoheimiyamoto/main.go
+++ kadai3-2/yoheimiyamoto/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
@@ -9,7 +10,7 @@ import (
 
 func main() {
 	c := downloader.NewClient("https://images.pexels.com/photos/248304/pexels-photo-248304.jpeg")
-	err := c.Download()
+	err := c.Download(context.Background())
 	if err != nil {
 		fmt.Fprintln(os.Stdout, err.Error())
 	}

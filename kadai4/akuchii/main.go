d191ee37031d5dfef8983b1b40fbe786b9abc85f
hakuta <test>
2018-12-04 23:39:33 +0900

    enable to change server port

--- kadai4/akuchii/main.go
+++ kadai4/akuchii/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/gopherdojo/dojo4/kadai4/akuchii/fortune"
 )
 
+// servert port
+var port int
+
+func init() {
+	flag.IntVar(&port, "p", 8080, "server port")
+}
+
 func main() {
+	flag.Parse()
 	f := fortune.NewFortune(fortune.DefaultClock{})
 	http.HandleFunc("/", f.Handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }

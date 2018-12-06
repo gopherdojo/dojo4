11278ad5693375ff5edb7c5b89f1716f53d365c9
uobikiemukot <uobikiemukot@gmail.com>
2018-12-06 16:40:28 +0900

    refactor: use subtest

--- kadai2/uobikiemukot/imgconv/imgconv_test.go
+++ kadai2/uobikiemukot/imgconv/imgconv_test.go
@@ -12,21 +12,25 @@ import (
 
 func TestSearch(t *testing.T) {
 	data := []struct {
+		name   string
 		config imgconv.Converter
 		root   string
 		expect []string
 	}{
 		{
+			name:   "search png",
 			config: imgconv.Converter{InputFormat: "png"},
 			root:   "testdata/subdir/a",
 			expect: []string{"testdata/subdir/a/1.png", "testdata/subdir/a/c/2.png"},
 		},
 		{
+			name:   "search jpg",
 			config: imgconv.Converter{InputFormat: "jpg"},
 			root:   "testdata/subdir/a",
 			expect: []string{"testdata/subdir/a/1.jpg", "testdata/subdir/a/c/2.jpg"},
 		},
 		{
+			name:   "search gif",
 			config: imgconv.Converter{InputFormat: "gif"},
 			root:   "testdata/subdir/b",
 			expect: []string{"testdata/subdir/b/3.gif"},
@@ -34,42 +38,72 @@ func TestSearch(t *testing.T) {
 	}
 
 	for _, d := range data {
-		result, err := imgconv.ExportSearch(d.root, d.config.InputFormat)
-		if err != nil {
-			t.Errorf("search() failed: %s\n", err)
-		}
-		if !reflect.DeepEqual(result, d.expect) {
-			t.Errorf("result unmatch: want(%v) got(%v)\n", d.expect, result)
-		}
+		d := d
+		t.Run(d.name, func(t *testing.T) {
+			result, err := imgconv.ExportSearch(d.root, d.config.InputFormat)
+			if err != nil {
+				t.Errorf("search() failed: %s\n", err)
+			}
+			if !reflect.DeepEqual(result, d.expect) {
+				t.Errorf("result unmatch: want(%v) got(%v)\n", d.expect, result)
+			}
+		})
 	}
 }
 
 func TestDecode_Success(t *testing.T) {
-	data := []string{
-		"testdata/subdir/a/1.png",
-		"testdata/subdir/a/c/2.jpg",
-		"testdata/subdir/b/3.gif",
+	data := []struct {
+		name string
+		path string
+	}{
+		{
+			name: "decode png",
+			path: "testdata/subdir/a/1.png",
+		},
+		{
+			name: "decode jpg",
+			path: "testdata/subdir/a/c/2.jpg",
+		},
+		{
+			name: "decode gif",
+			path: "testdata/subdir/b/3.gif",
+		},
 	}
 
-	for _, input := range data {
-		_, err := imgconv.ExportDecode(input)
-		if err != nil {
-			t.Errorf("decode(%s) failed: %s\n", input, err)
-		}
+	for _, d := range data {
+		d := d
+		t.Run(d.name, func(t *testing.T) {
+			_, err := imgconv.ExportDecode(d.path)
+			if err != nil {
+				t.Errorf("decode(%s) failed: %s\n", d.path, err)
+			}
+		})
 	}
 }
 
 func TestDecode_Failure(t *testing.T) {
-	data := []string{
-		"file/not/found.png",
-		"testdata/subdir",
+	data := []struct {
+		name string
+		path string
+	}{
+		{
+			name: "file not exist",
+			path: "file/not/found.png",
+		},
+		{
+			name: "non image file",
+			path: "testdata/subdir",
+		},
 	}
 
-	for _, input := range data {
-		_, err := imgconv.ExportDecode(input)
-		if err == nil {
-			t.Errorf("decode(%s) succeeded (must fail)\n", input)
-		}
+	for _, d := range data {
+		d := d
+		t.Run(d.name, func(t *testing.T) {
+			_, err := imgconv.ExportDecode(d.path)
+			if err == nil {
+				t.Errorf("decode(%s) succeeded (must fail)\n", d.path)
+			}
+		})
 	}
 }
 
@@ -85,20 +119,35 @@ func testCreateImage(t *testing.T, path string) image.Image {
 }
 
 func TestEncode_Success(t *testing.T) {
-	data := []imgconv.Converter{
-		{InputFormat: "png", OutputFormat: "jpg"},
-		{InputFormat: "jpg", OutputFormat: "gif"},
-		{InputFormat: "gif", OutputFormat: "png"},
+	data := []struct {
+		name      string
+		converter imgconv.Converter
+	}{
+		{
+			name:      "convert from png to jpg",
+			converter: imgconv.Converter{InputFormat: "png", OutputFormat: "jpg"},
+		},
+		{
+			name:      "convert from jpg to gif",
+			converter: imgconv.Converter{InputFormat: "jpg", OutputFormat: "gif"},
+		},
+		{
+			name:      "convert from gif to png",
+			converter: imgconv.Converter{InputFormat: "gif", OutputFormat: "png"},
+		},
 	}
 
 	output := "testdata/converted_image.jpg"
 	img := testCreateImage(t, "testdata/subdir/a/1.png")
 
-	for _, c := range data {
-		err := imgconv.ExportConverterEncode(&c, output, img)
-		if err != nil {
-			t.Errorf("encode() failed: %s\n", err)
-		}
+	for _, d := range data {
+		d := d
+		t.Run(d.name, func(t *testing.T) {
+			err := imgconv.ExportConverterEncode(&d.converter, output, img)
+			if err != nil {
+				t.Errorf("encode() failed: %s\n", err)
+			}
+		})
 	}
 	os.Remove(output)
 }
